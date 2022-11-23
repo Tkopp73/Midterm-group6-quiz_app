ff05f9cb241af49064943c7828bd5aeb9b9556f6
Tim Kopplin <timkopplin@hotmail.com>
2022-11-23 18:26:46 +0000

    adding some css to make the pages look nice added some js to make it a little more functional

---
+++ public/scripts/api-quiz.js
@@ -0,0 +1,30 @@
+
+
+
+$(document).ready(function() {
+  // console.log("Volleyball")
+  let counterBuffer = 0;
+  const button = $('.answers');
+  let $p = $('<p>');
+  let text = `${counterBuffer}/10`;
+  const handleClick = () => {
+    // console.log('click', event.target);
+    if (event.target.value === 'right') {
+      $(event.target).css('background', '#2AFA00');
+      $(event.target).attr("disabled", true);
+      counterBuffer += 1;
+      text = `${counterBuffer}/10`;
+      $p.text(text);
+      $('#result').empty();
+      $('#result').append($p);
+    } else {
+      $(event.target).css('background', '#FA1503');
+      $(event.target).attr("disabled", true);
+    }
+  };
+
+  button.click(handleClick);
+
+
+});
+
